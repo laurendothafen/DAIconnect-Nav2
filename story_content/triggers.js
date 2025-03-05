b895395d684998a943df1193b3e17dec82ff3de1
laurendothafen <laurendothafen@gmail.com>
2025-03-05 14:21:51 -0500

    practice navigating DAIconnect updates

---
+++ story_content/triggers.js
@@ -0,0 +1,395 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ZP459BxyOU":
+        Script1();
+        break;
+      case "5duGiLijDIl":
+        Script2();
+        break;
+      case "5isPDQ4Yjjo":
+        Script3();
+        break;
+      case "6r2qpLjQpzI":
+        Script4();
+        break;
+      case "6XiREo4UIKb":
+        Script5();
+        break;
+      case "5kJ8Dn6X18z":
+        Script6();
+        break;
+      case "60OAFKetsnW":
+        Script7();
+        break;
+      case "5aNZDKa22fW":
+        Script8();
+        break;
+      case "6ADUR8sSt6X":
+        Script9();
+        break;
+      case "6IecSEcy5Pj":
+        Script10();
+        break;
+      case "6XulQtvhu9P":
+        Script11();
+        break;
+      case "5rJq8abXbkh":
+        Script12();
+        break;
+      case "66Lr86BfDJh":
+        Script13();
+        break;
+      case "6lf1vJVjZuh":
+        Script14();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('6AMfK38q06H');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6hTYoyxfyuV';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6AMfK38q06H');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6hTYoyxfyuV';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('61a92zO4nA9');
+const duration = 2000;
+const easing = 'ease-out';
+const id = '5XPtcpSAW8z';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script4 = function()
+{
+  const target = object('6mHsR3cauAI');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6GLO2Q6thnR';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script5 = function()
+{
+  const target = object('5iqaofxvrDX');
+const duration = 5000;
+const easing = 'ease-out';
+const id = '5sebAmuORXe';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script6 = function()
+{
+  const target = object('5fe8PK1HVYy');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5lTZZwUt7pd';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script7 = function()
+{
+  const target = object('5xJgOHnZO7m');
+const duration = 750;
+const easing = 'ease-out';
+const id = '62WoicEUnUV';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script8 = function()
+{
+  const target = object('6ZkzLB0M7zA');
+const duration = 2000;
+const easing = 'ease-out';
+const id = '6XPnzmLt2zU';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script9 = function()
+{
+  const target = object('6ZkzLB0M7zA');
+const duration = 2000;
+const easing = 'ease-out';
+const id = '6XPnzmLt2zU';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script10 = function()
+{
+  const target = object('6HTwMRRAyMO');
+const duration = 1500;
+const easing = 'ease-out';
+const id = '6Q6tU40W7aW';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script11 = function()
+{
+  const target = object('67jRvrzZOpD');
+const duration = 2000;
+const easing = 'ease-out';
+const id = '5hc4PsiQ0YZ';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script12 = function()
+{
+  const target = object('681Q5ShkVDU');
+const duration = 1500;
+const easing = 'ease-out';
+const id = '6BhduKfnNbq';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script13 = function()
+{
+  const target = object('681Q5ShkVDU');
+const duration = 1500;
+const easing = 'ease-out';
+const id = '6BhduKfnNbq';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script14 = function()
+{
+  const target = object('6MlMuMlN6mp');
+const duration = 2500;
+const easing = 'ease-out';
+const id = '6Tk5El80MyX';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
